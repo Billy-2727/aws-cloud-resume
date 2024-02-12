230d2ee31f8c5a3fae91c0786a3c7e6874cd8678
billy-2727 <bilza7607@gmail.com>
2024-02-12 22:05:22 +0000

    fixed js error and updated github link

--- website/index.js
+++ website/index.js
@@ -1,7 +1,6 @@
 // fetch api data for the view function
 const counter = document.querySelector(".view-counter");
 async function updateCounter() {
-    mode: 'no-cors'
     let response = await fetch("https://zrsqcovodn3lzhg5kirtjm6ziq0swedm.lambda-url.eu-west-2.on.aws/");
     let data = await response.json();
     counter.innerHTML = ` Views: ${data}`;
