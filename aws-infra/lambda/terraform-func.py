8b03727731ef44afc48c7217ec5b84870adc7a68
billy-2727 <bilza7607@gmail.com>
2024-02-18 11:52:38 +0000

    test

--- aws-infra/lambda/terraform-func.py
+++ aws-infra/lambda/terraform-func.py
@@ -1,20 +1,13 @@
 import json
 import boto3
 
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('cloudresume')
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("cloudresume")
+
 
 def lambda_handler(event, context):
-    response = table.get_item(Key={
-        'id':'0'
-    })
-    views = response['Item']['views']
+    response = table.get_item(Key={"id": "0"})
+    views = response["Item"]["views"]
     views = views + 1
     print(views)
-    response = table.put_item(Item={
-        'id':'0',
-        'views': views
-    })
-    
-    return views
- 
+    response = table.put_item(Item={"id": "0", "views": views})
